38a455d12ee95991028ec15b36b6e22b6174592f
Jason Schweier <jasons@nulogy.com>
2019-08-29 21:45:53 -0400

    Refactor to table-driven test for tokenizing

--- main_test.go
+++ main_test.go
@@ -6,12 +6,25 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	toml := "key = \"value\""
+	testcases := []struct {
+		Description string
+		Toml        string
+		Tokens      []Token
+	}{
+		{
+			Description: "A simple key-value",
+			Toml:        "key = \"value\"",
+			Tokens:      []Token{BARE_KEY, WHITESPACE, ASSIGNMENT, WHITESPACE, STRING, EOF},
+		},
+	}
 
-	want := []Token{BARE_KEY, WHITESPACE, ASSIGNMENT, WHITESPACE, STRING, EOF}
-	got := Parse(toml)
+	for _, tc := range testcases {
+		t.Run(tc.Description, func(t *testing.T) {
+			got := Parse(tc.Toml)
 
-	if !reflect.DeepEqual(want, got) {
-		t.Errorf("Wanted %v, but got %v", want, got)
+			if !reflect.DeepEqual(tc.Tokens, got) {
+				t.Errorf("Wanted %v, but got %v", tc.Tokens, got)
+			}
+		})
 	}
 }
